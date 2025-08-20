dacadba98a049633ff18f838e2d1d02c6ef59625
Paola Andrea Vallejo Correa <pvallej3@eafit.edu.co>
2025-08-20 13:23:28 -0500

    Nueva app news

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -15,18 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from movie import views as movieViews
 
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', movieViews.home),
-
+    path('admin/', admin.site.urls), 
+    #path('', movieViews.home),
+    path('', movieViews.home, name='home'),
     # Ruta para una página 'About'
-    path('about/', movieViews.about),
+    #path('about/', movieViews.about),
+    path('about/', movieViews.about, name='about'),
+    path('news/', include('news.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
